2324c33eb3e91f3c028af827b83f8a018f731b6b
Paulo Gouveia <paulojlgouveia@gmail.com>
2017-04-03 10:42:49 +0100

    addIntetrest activity

--- LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/AddInterestActivity.java
+++ LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/AddInterestActivity.java
@@ -3,39 +3,36 @@ package pt.ulisboa.tecnico.cmov.locmess;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import pt.ulisboa.tecnico.cmov.locmess.Exceptions.StorageException;
-import pt.ulisboa.tecnico.cmov.locmess.Tasks.GetLocationsTask;
-import pt.ulisboa.tecnico.cmov.locmess.Tasks.GetUserProfileTask;
+import pt.ulisboa.tecnico.cmov.locmess.Tasks.AddInterestTask;
 
-public class ProfileActivity extends AppCompatActivity {
+public class AddInterestActivity extends AppCompatActivity {
 
     private long _sessionId = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-    }
-
-    @Override
-    protected void onResume(){
-        super.onResume();
-
         try {
             _sessionId = DataManager.getInstance().getSessionId(getApplicationContext());
-
-        } catch(StorageException e) {
+        }catch (StorageException e){
             Toast.makeText(this, R.string.sessionFailed, Toast.LENGTH_SHORT).show();
             Intent i = new Intent(this, LoginActivity.class);
             startActivity(i);
         }
-
-        (new GetUserProfileTask(this, _sessionId)).execute();
+        setContentView(R.layout.activity_add_interest);
     }
 
+    public void submitInterest(View view) {
+        String key = ((EditText) findViewById(R.id.editTextKey)).getText().toString();
+        String value = ((EditText) findViewById(R.id.editTextValue)).getText().toString();
 
-
+        (new AddInterestTask(this, _sessionId, key, value)).execute();
+        finish();
+    }
 
 }
--- LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/Tasks/GetUserProfileTask.java
+++ LocMess/app/src/main/java/pt/ulisboa/tecnico/cmov/locmess/Tasks/GetUserProfileTask.java
@@ -1,8 +1,17 @@
 package pt.ulisboa.tecnico.cmov.locmess.Tasks;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -10,11 +19,14 @@ import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.web.client.RestClientException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import pt.ulisboa.tecnico.cmov.locmess.Domain.Location;
 import pt.ulisboa.tecnico.cmov.locmess.HomeActivity;
+import pt.ulisboa.tecnico.cmov.locmess.MessageViewActivity;
 import pt.ulisboa.tecnico.cmov.locmess.R;
 import pt.ulisboa.tecnico.cmov.locmess.Responses.Cookie;
 import pt.ulisboa.tecnico.cmov.locmess.Responses.InterestsList;
@@ -68,17 +80,56 @@ public class GetUserProfileTask extends RestTask{
         }
     }
 
+
     @Override
     protected void onPostExecute(String result){
         Toast.makeText(_context, result, Toast.LENGTH_SHORT).show();
+
         if(_successful) {
-            /*TextView text = (TextView) _context.findViewById(R.id.debugText);
-            text.setText("");
-            for (Map.Entry<String, String> entry : _interests.entrySet()) {
-                text.setText(text.getText() + entry.getKey() + " : " + entry.getValue() + "\n");
-            }*/
+//            ListView list = (ListView) _context.findViewById(R.id.listViewInterests);
+//
+//            ListAdapter interestsAdapter = new GetUserProfileTask.InterestAdapter(_context, _interests);
+//
+//            list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//                @Override
+//                public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//                    Toast.makeText(_context, "selected something", Toast.LENGTH_SHORT).show();
+//
+////                    Intent intent = new Intent(_context, MessageViewActivity.class);
+////                    intent.putExtra("SESSIONID", _sessionId);
+////                    _context.startActivity(intent);
+//                }
+//            });
+//
+//            list.setAdapter(interestsAdapter);
+//
+
         }
     }
+
+//    // class for the custom display of locations' list
+//    protected class InterestAdapter extends ArrayAdapter<Map.Entry<String,String>> {
+//
+//        public InterestAdapter(@NonNull Context context, Map<String,String> interests) {
+//            super(context, R.layout.interest_item, new ArrayList<Map.Entry<String,String>>(interests.values()));
+//        }
+//
+//        @Override
+//        public View getView(int position, View convertView, ViewGroup parent) {
+//            LayoutInflater interestInflater = LayoutInflater.from(getContext());
+//            View rowView = interestInflater.inflate(R.layout.interest_item, parent, false);
+//
+//            Map.Entry<String,String> interest = getItem(position);
+//            TextView key = (TextView) rowView.findViewById(R.id.textViewKey);
+//            TextView value = (TextView) rowView.findViewById(R.id.textViewValue);
+//
+//            key.setText(interest.getKey());
+//            value.setText(interest.getValue());
+//
+//            return rowView;
+//        }
+//
+//    }
 
 }
 
