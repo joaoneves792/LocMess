60c6bcf645e2cc9984c722bae210e4b66c70b36b
Joao Neves <joaoneves792@gmail.com>
2017-05-07 12:39:25 +0100

    SSL on server

--- LocMessServer/src/main/java/LocMess/ServerApp.java
+++ LocMessServer/src/main/java/LocMess/ServerApp.java
@@ -9,6 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ServerApp {
     public static void main(String[] args) {
+        System.setProperty("server.port", "8080");
+        System.setProperty("server.ssl.key-store", "classpath:keystore.jks");
+        System.setProperty("server.ssl.key-store-password", "123456");
+        System.setProperty("server.ssl.key-password", "123456");
+        System.setProperty("server.ssl.enabled", "true");
+
         SpringApplication.run(ServerApp.class, args);
     }
 }
